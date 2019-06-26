b90acc3ccf0e97535475b2bdea939d2b3d47ac2b
dhebbar7 <d.hebbar7@gmail.com>
2019-06-26 17:28:51 +0530

    update

--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,7 +1,16 @@
 import React, { Component } from "react";
 import { ListGroup, ListGroupItem, Button } from "reactstrap";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
+import uuid from "uuid";
 class ShoppingList extends Component {
+  state = {
+    items: [
+      { id: uuid(), name: "Laptop" },
+      { id: uuid(), name: "Mouse" },
+      { id: uuid(), name: "Keyboard" },
+      { id: uuid(), name: "Laptop" }
+    ]
+  };
   render() {
     return <div />;
   }
