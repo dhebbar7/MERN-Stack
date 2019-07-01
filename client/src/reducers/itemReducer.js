c4c21661dd65adcca1afeeed5517d122b935b8fa
dhebbar7 <d.hebbar7@gmail.com>
2019-07-01 12:41:42 +0530

    Redux Implementation

---
+++ client/src/reducers/itemReducer.js
@@ -0,0 +1,9 @@
+import uuid from "uuid";
+const initialState = {
+  items: [
+    { id: uuid(), name: "Laptop" },
+    { id: uuid(), name: "Mouse" },
+    { id: uuid(), name: "Keyboard" },
+    { id: uuid(), name: "Monitor" }
+  ]
+};
